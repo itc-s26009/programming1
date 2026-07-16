08e397e9f96b37ea42d38c3e34ef0ba75c4305e7
itc-s26009 <s26009@std.it-college.ac.jp>
2026-07-16 03:24:27 +0000

    追加

---
+++ chap04/q4_6_2.py
@@ -0,0 +1,26 @@
+import random
+def generate_students_data(num_students=10):
+    students_data = []
+    for i in range(num_students):
+        name = 'n' + str(random.randint(10, 50))
+        height = random.randint(150,190)
+        weight = random.randint(50,80)
+        students_data.append((name, height, weight))
+        if i == 0: print('i, name, height, weight')
+        if i < 2 or i == num_students - 1:
+            print(i, name, height, weight)
+        elif i == 2:
+            print('...')
+    return students_data
+
+students_data = generate_students_data(10)
+
+students_by_height = sorted(students_data, key=lambda s: s[1])
+print('\nSort by height')
+for student in students_by_height:
+    print(student)
+
+students_by_weight = sorted(students_data, key=lambda s: s[1])                          
+print('\nSort by weight')                                                               
+for student in students_by_weight:                                                      
+    print(student) 
