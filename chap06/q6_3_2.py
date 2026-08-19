47bffaa6c35a587e8fb5cf0b6e2cc5a693a06ea1
itc-s26009 <s26009@std.it-college.ac.jp>
2026-08-19 01:50:30 +0000

    6.3

--- chap06/q6_3_2.py
+++ chap06/q6_3_2.py
@@ -1,8 +1,7 @@
 class Rectangle:
-    '''長方形'''
     angle = 90
 
-    def __init__(self, width, height):
+    def __init__(self,width, height):
         self.name = 'rectangle'
         self.width = width
         self.height = height
@@ -26,17 +25,17 @@ class Rectangle:
         h = self.height
         p = self.perimeter
         a = self.area
-
-        print("name: {}, width: {}, height: {}, angle: {}".format(n, w, h, ang))
+        print("name: {}, widht: {}, height: {}, angle {}".format(n, w, h, ang))
         print("perimeter: {}, area: {}".format(p, a))
 
-# インスタンスを作って実行
-r1 = Rectangle(4, 3)
-r1.show_attributes()
+#r1 = Rectangle(4, 3)
+#r1.show_attributes()
 
 class Square(Rectangle):
-    '''正方形'''
 
     def __init__(self, width):
         super().__init__(width, width)
         self.name = 'square'
+
+s1 = Square(4)
+s1.show_attributes()
